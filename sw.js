299fd529e6397dfe39097e1216a6c92694edd666
Jackary <jack.kochen@gmail.com>
2021-01-25 15:07:35 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-881f80bcb511693077bc.js"
+    "url": "webpack-runtime-e1c860a42dfc9e2e0285.js"
   },
   {
     "url": "framework-06f646d936b841f99ebd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3d85ca58622365579616c7b1cfdd53c7"
+    "revision": "d5c32c0f0d21cc5781e8b1b5b9165283"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9bdb3ff409b023b64b9b80efab742bdc"
+    "revision": "026ec8ce9d336d2a22559bb7741183a8"
   },
   {
     "url": "polyfill-c7e713c367b62bf611ed.js"
